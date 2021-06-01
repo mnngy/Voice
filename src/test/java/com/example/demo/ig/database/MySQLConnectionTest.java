e54cded80cd28ff4071d6aa3c3b9153232af1700
mnngy <59257197+mnngy@users.noreply.github.com>
2021-06-01 17:52:24 +0900

    테스크 코드 수정

--- src/test/java/com/example/demo/ig/database/MySQLConnectionTest.java
+++ src/test/java/com/example/demo/ig/database/MySQLConnectionTest.java
@@ -8,23 +8,23 @@ import java.sql.SQLException;
 
 public class MySQLConnectionTest {
 
-    String driver = "com.mysql.cj.jdbc.Driver";
-    String url = "jdbc:mysql://localhost:3306/voice?serverTimezone=UTC&characterEncoding=UTF-8";
-    String id = "root";
-    String pw = "miningyu";
-
-    @Test
-    void testConnection() {
-        try {
-            Class.forName(driver);
-            try {
-                Connection conn = DriverManager.getConnection(url, id, pw);
-                System.out.println(conn);
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+//    String driver = "com.mysql.cj.jdbc.Driver";
+//    String url = "jdbc:mysql://localhost:3306/voice?serverTimezone=UTC&characterEncoding=UTF-8";
+//    String id = "root";
+//    String pw = "miningyu";
+//
+//    @Test
+//    void testConnection() {
+//        try {
+//            Class.forName(driver);
+//            try {
+//                Connection conn = DriverManager.getConnection(url, id, pw);
+//                System.out.println(conn);
+//            } catch (SQLException throwables) {
+//                throwables.printStackTrace();
+//            }
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
