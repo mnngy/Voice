e0578cc609a9a53770a61416f8674258f198c3aa
mnngy <59257197+mnngy@users.noreply.github.com>
2021-11-29 16:58:02 +0900

    memberSelectCountById mybatis annotation -> mybatis xml

--- src/main/java/com/example/demo/ig/mapper/MemberMapper.java
+++ src/main/java/com/example/demo/ig/mapper/MemberMapper.java
@@ -2,11 +2,9 @@ package com.example.demo.ig.mapper;
 
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MemberMapper {
 
-    @Select("select count(*) from member where memberId = #{memberId}")
     int memberSelectCountById(String memberId);
 }
